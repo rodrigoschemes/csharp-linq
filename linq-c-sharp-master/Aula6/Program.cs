0b9b7660f0d82633da58ffd1fa0b99c6c8bd2991
rodrigoschemes <rodrigoschemes@yahoo.com.br>
2017-12-20 01:00:43 -0200

    first commit

---
+++ linq-c-sharp-master/Aula6/Program.cs
@@ -0,0 +1,82 @@
+﻿using AluraTunes.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula6
+{
+    /// <summary>
+    /// Contando e Totalizando valores
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var contexto = new AluraTunesEntities())
+            {
+                var query = from f in contexto.Faixas
+                            where f.Album.Artista.Nome == "Led Zeppelin"
+                            select f;
+
+                var quantidade = contexto.Faixas
+                                .Count(f => f.Album.Artista.Nome == "Led Zeppelin");
+
+                Console.WriteLine("Led Zeppelin tem {0} faixas de música.", quantidade);
+
+                Console.WriteLine();
+
+
+
+
+
+                var query2 = from inf in contexto.ItensNotaFiscal
+                            where inf.Faixa.Album.Artista.Nome == "Led Zeppelin"
+                            select new { totalDoItem = inf.Quantidade * inf.PrecoUnitario };
+
+                var totalDoArtista = query2.Sum(q => q.totalDoItem);
+
+                Console.WriteLine("Total do artista: R$ {0}", totalDoArtista);
+
+                Console.WriteLine();
+
+
+
+
+
+
+                var query3 = from inf in contexto.ItensNotaFiscal
+                             where inf.Faixa.Album.Artista.Nome == "Led Zeppelin"
+                            group inf by inf.Faixa.Album into agrupado
+                            let vendasPorAlbum = agrupado.Sum(a => a.Quantidade * a.PrecoUnitario)
+                            orderby vendasPorAlbum
+                                descending
+                            select new
+                            {
+                                TituloDoAlbum = agrupado.Key.Titulo,
+                                TotalPorAlbum = vendasPorAlbum
+                            };
+
+                foreach (var agrupado in query3)
+                {
+                    Console.WriteLine("{0}\t{1}",
+                        agrupado.TituloDoAlbum.PadRight(40),
+                        agrupado.TotalPorAlbum);
+                }
+
+                Console.WriteLine();
+
+
+
+
+
+
+
+                contexto.Database.Log = Console.WriteLine;
+
+                
+            }
+        }
+    }
+}
---
+++ linq-c-sharp-master/Aula7/Program.cs
@@ -0,0 +1,87 @@
+﻿using AluraTunes.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula7
+{
+    /// <summary>
+    /// Max, Min, Média e Funções Customizadas
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var contexto = new AluraTunesEntities())
+            {
+                var maiorVenda = contexto.NotasFiscais.Max(nf => nf.Total);
+                var menorVenda = contexto.NotasFiscais.Min(nf => nf.Total);
+                var vendaMedia = contexto.NotasFiscais.Average(nf => nf.Total);
+
+                Console.WriteLine("A maior venda é de R$ {0}", maiorVenda);
+                Console.WriteLine("A menor venda é de R$ {0}", menorVenda);
+                Console.WriteLine("A venda média é de R$ {0}", vendaMedia);
+
+                var vendas = (from nf in contexto.NotasFiscais
+                              group nf by 1 into agrupado
+                              select new
+                              {
+                                  maiorVenda = agrupado.Max(nf => nf.Total),
+                                  menorVenda = agrupado.Min(nf => nf.Total),
+                                  vendaMedia = agrupado.Average(nf => nf.Total)
+                              }).Single();
+
+                Console.WriteLine("A maior venda é de R$ {0}", vendas.maiorVenda);
+                Console.WriteLine("A menor venda é de R$ {0}", vendas.menorVenda);
+                Console.WriteLine("A venda média é de R$ {0}", vendas.vendaMedia);
+
+
+
+                var vendaMediana = Mediana(contexto.NotasFiscais.Select(ag => ag.Total));
+                Console.WriteLine("Venda Mediana: R$ {0}", vendaMediana);
+
+                vendaMediana = contexto.NotasFiscais.Mediana(ag => ag.Total);
+                Console.WriteLine("Venda Mediana: R$ {0}", vendaMediana);
+            }
+
+            Console.ReadKey();
+        }
+
+        public static decimal Mediana(IQueryable<decimal> origem)
+        {
+            int contagem = origem.Count();
+            var ordenado = origem.OrderBy(p => p);
+
+            var elementoCentral_1 = ordenado.Skip((contagem - 1) / 2).First();
+            var elementoCentral_2 = ordenado.Skip(contagem / 2).First();
+
+            decimal mediana = (elementoCentral_1 + elementoCentral_2) / 2;
+
+            return mediana;
+        }
+
+    }
+
+    public static class Extensions
+    {
+        public static decimal Mediana<TSource>(this IQueryable<TSource> origem, Expression<Func<TSource, decimal>> selector)
+        {
+            int contagem = origem.Count();
+
+            var funcSeletor = selector.Compile();
+            var ordenado = origem
+                .Select(selector)
+                .OrderBy(x => x);
+
+            var elementoCentral_1 = ordenado.Skip((contagem - 1) / 2).First();
+            var elementoCentral_2 = ordenado.Skip(contagem / 2).First();
+
+            decimal mediana = (elementoCentral_1 + elementoCentral_2) / 2;
+
+            return mediana;
+        }
+    }
+}
